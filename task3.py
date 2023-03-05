f0e34cc81c22a30abc3995cb5cc38ff800cb95bd
Alla_Dseshulska <dzeshulska@gmail.com>
2023-03-05 11:38:20 +0200

    added file

---
+++ task3.py
@@ -0,0 +1,7 @@
+number_us = int(input("Enter any number! :"))
+if number_us > 0:
+    print("Number is positiv")
+if number_us < 0:
+    print("Number is negative")
+if number_us == 0:
+    print("Number is equal to zero")
